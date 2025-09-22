d340f60219f27158b7cd02ee820bebff3d34e758
suseliwen <susanne.wenblad@gmail.com>
2025-09-22 19:07:54 +0200

    Tested median imputation vs. dropping columns for numeric null values. Decided to continue with dropped-column dataset since it yields better linear regression predictions despite reduced data size.

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -3,4 +3,5 @@ from importlib.resources import files
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/cleaned_taxi_trip_pricing.csv") #absolute path to the csv-file in data
 
+
 print(TAXI_CSV_PATH)
